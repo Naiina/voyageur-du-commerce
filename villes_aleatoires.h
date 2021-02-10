9107c6cf0f6edcce91a4e7ab9634210ddcdf016d
alixquintin <62743004+alixquintin@users.noreply.github.com>
2021-02-10 09:38:22 +0100

    Update villes_aleatoires.h

--- villes_aleatoires.h
+++ villes_aleatoires.h
@@ -1,9 +1,16 @@
 #ifndef VILLES_ALEATOIRES_H_INCLUDED
 #define VILLES_ALEATOIRES_H_INCLUDED
+#include "ville.h"
+#include <vector>
+#include <ctime>
+#include <stdlib.h>
+#include <iostream>
+#include <random>
 
+using namespace std;
 
 // Retourne un tableau de villes
-Ville* villes_aleatoires(int i);
+vector<Ville> villes_aleatoires(int k);
 
 
 #endif // VILLES_ALEATOIRES_H_INCLUDED
