afb152d46da4965dc88c32b09db989c7b4a12bd8
Yue0925 <60290328+Yue0925@users.noreply.github.com>
2021-02-11 08:15:21 +0100

    Delete include directory

--- include/mutation.h
+++
@@ -1,7 +0,0 @@
-#ifndef MUTATION_H_INCLUDED
-#define MUTATION_H_INCLUDED
-
-
-Individu mutation(Individu I, float alpha);
-
-#endif // MUTATION_H_INCLUDED
--- include/villes_aleatoires.h
+++
@@ -1,9 +0,0 @@
-#ifndef VILLES_ALEATOIRES_H_INCLUDED
-#define VILLES_ALEATOIRES_H_INCLUDED
-
-
-// Retourne un tableau de villes
-Ville* villes_aleatoires(int i);
-
-
-#endif // VILLES_ALEATOIRES_H_INCLUDED
