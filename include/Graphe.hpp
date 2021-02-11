8e896f21d97b71aab67f9985f1e07a424a5b4cb9
Yue0925 <60290328+Yue0925@users.noreply.github.com>
2021-02-11 08:50:04 +0100

    update Graphe

--- include/Graphe.hpp
+++ include/Graphe.hpp
@@ -12,6 +12,8 @@ public:
     Graphe();
     bool hasAnEdge(const int u, const int v) const; // on considère (u,v),(v,u) sont équivalentes
     void addAnEdge(const Ville &, const Ville &); // ajout arrête a---b avec la distance entre a et b
+    map<pair<int, int>, float>::const_iterator begin() const;
+    map<pair<int, int>, float>::const_iterator end() const;
 };
 
 /*
--- src/Graphe.cpp
+++ src/Graphe.cpp
@@ -22,11 +22,24 @@ void Graphe::addAnEdge(const Ville & v1, const Ville & v2){
     graphe.insert({{v1.getIdVille(), v2.getIdVille()}, dist(v1, v2)});
 }
 
+map<pair<int, int>, float>::const_iterator Graphe::begin() const{
+    return graphe.begin();
+}
+
+map<pair<int, int>, float>::const_iterator Graphe::end() const{
+    return graphe.end();
+}
+
 /*
 * foctions de classe
 */
 
 ostream& operator<<(ostream& os, const Graphe & graphe){
     os<<"Graphe"<<endl;
+    map<pair<int, int>, float>::const_iterator it;
+    for ( it = graphe.begin(); it != graphe.end(); it++)
+    {
+        os<<it->first.first<<"---"<<it->first.second<<" : "<<it->second<<endl;
+    }
     return os;
 }
