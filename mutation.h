295355c89a46dd347c88d985d8480bbbd2945fa3
alixquintin <62743004+alixquintin@users.noreply.github.com>
2021-02-11 11:33:05 +0100

    Update mutation.h

--- mutation.h
+++ mutation.h
@@ -1,7 +1,11 @@
 #ifndef MUTATION_H_INCLUDED
 #define MUTATION_H_INCLUDED
 
+#include "chemin.h"
 
-Individu mutation(Individu I, float alpha);
+// Car en C++ (-1)%5 = -1 et pas 4
+int modulo (int i, int j);
+
+Chemin mutation(Chemin I, float alpha);
 
 #endif // MUTATION_H_INCLUDED
