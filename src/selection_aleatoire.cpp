99c47d270d3f96913d79ab9fd8e9a5b82763b9dd
Nina Gregorio <nina@test.com>
2021-02-24 19:51:11 +0100

    Amélioration sélection aléatoire

--- src/selection_aleatoire.cpp
+++ src/selection_aleatoire.cpp
@@ -7,18 +7,17 @@
 
 Population selection_aleatoire(int q, Population & pop){ //q doit etre compris entre 0 et taille_pop
     srand (time(NULL));
-    int taille_pop=pop.taille;
+    int taille_pop=pop.getTaille();
     Population new_pop(taille_pop);
-    new_pop.taille=taille_pop;
     int t=0;
     for(int i=0;i<q;i++){
         int k=rand()%taille_pop;
-        new_pop.parents[t]=pop.parents[k];
+        new_pop.setParent(t,pop.getParents()[k]);
         t++;
     }
     for(int i=q;i<taille_pop;i++){
         int k=rand()%taille_pop;
-        new_pop.parents[t]=pop.enfants[k];
+        new_pop.setParent(t,pop.getEnfants()[k]);
         t++;
     }
     //valmin
