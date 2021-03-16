b6f6cb1026b7d024f78e8b0556d78d2bf80bddcd
JustineDeSousa <justine.desousa6@gmail.com>
2021-03-16 16:37:57 +0100

    Test berlin RANG

--- test/main.m
+++ test/main.m
@@ -1,6 +1,6 @@
 clear; clc; close('all');
 monGraphe = 'berlin52';
-choix = 0;
+choix = 1;
 plot_graphe(monGraphe,".algo.tour",choix);
 plot_distance(monGraphe,choix);
 plot_graphe(monGraphe,".opt.tour");
